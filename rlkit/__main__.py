0cd91a5c85a581508dc31c4a90adfb855b8221e9
Shubham Jha <sjha@indeed.com>
2020-06-20 09:35:34 +0530

    Change initilization syntax of params dict

--- rlkit/__main__.py
+++ rlkit/__main__.py
@@ -2,20 +2,23 @@ from rlkit.agents import RandomAgent
 from rlkit.environments.gym_environment import GymEnvironment
 from rlkit.trainers import BasicTrainer
 
-params = {
-    "environment_params": {
-        "env_name": "SpaceInvaders-v0",
-    },
-    "agent_params": {
-
-    },
-    "training_params": {
-        "run_name": "test_run",
-        "train_interval": 10,
-        "episodes": 5,
-        "steps": 500,
-    },
-}
+SEED = 1234
+params = dict(
+    environment_params = dict(
+        env_name = "SpaceInvaders-v0",
+        seed = SEED,
+    ),
+    agent_params = dict(
+        seed = SEED,
+    ),
+    training_params= dict(
+        run_name = "test_run",
+        train_interval = 10,
+        episodes = 5,
+        steps = 500,
+        seed = SEED,
+    ),
+)
 
 env = GymEnvironment(params["environment_params"])
 agent = RandomAgent(params["agent_params"], env.get_action_space())
